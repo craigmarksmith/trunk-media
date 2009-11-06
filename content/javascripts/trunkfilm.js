f5806178cf8756783a5dde69fa790a6cc9672023
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2009-11-06 10:08:23 +0000

    Scrolling works but is a little off centre

---
+++ content/javascripts/trunkfilm.js
@@ -0,0 +1,25 @@
+TrunkFilm = {}
+TrunkFilm.Movies = {
+  
+  init: function(){
+    $('#scroll-left').click(TrunkFilm.Movies.scrollLeft);
+    $('#scroll-right').click(TrunkFilm.Movies.scrollRight);
+  },
+  
+  scrollLeft: function(){
+    TrunkFilm.Movies.scroll('-=500px');
+  },
+  
+  scrollRight: function(){
+    TrunkFilm.Movies.scroll('+=500px');
+  },
+  
+  scroll: function(scroll_amount){
+    $('#movies').animate({
+      marginLeft: scroll_amount
+    }, 1000);
+  }
+}
+
+$(document).ready(TrunkFilm.Movies.init);
+
