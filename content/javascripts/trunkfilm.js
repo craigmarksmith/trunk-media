875ffe7e696a597493bc250d5bf60f613522b45e
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2009-11-07 18:49:16 +0000

    Selected the correct scroll blob

--- content/javascripts/trunkfilm.js
+++ content/javascripts/trunkfilm.js
@@ -6,27 +6,34 @@ TrunkFilm.Movies = {
     $('#scroll-left').click(TrunkFilm.Movies.scrollLeft);
     $('#scroll-right').click(TrunkFilm.Movies.scrollRight);
   },
-  
+
   scrollLeft: function(){
     if(current_position > 0){
       TrunkFilm.Movies.scroll('+=703px');
       current_position -= 1;
+      TrunkFilm.Movies.selectScrollBall(current_position, current_position+1);
     }
     return false;
   },
-  
+
   scrollRight: function(){
     if(current_position < ($('#movies object').size()-1)){
       TrunkFilm.Movies.scroll('-=703px');
       current_position += 1;
+      TrunkFilm.Movies.selectScrollBall(current_position, current_position-1);
     }
     return false;
   },
-  
+
   scroll: function(scroll_amount){
     $('#movies').animate({
       marginLeft: scroll_amount
     }, 1000);
+  },
+
+  selectScrollBall: function(current_position, previous_position){
+    $($('.scroll-ball')[current_position]).addClass('selected');
+    $($('.scroll-ball')[previous_position]).removeClass('selected');
   }
 }
 
